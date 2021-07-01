0fc0d13c402116a7c286eb046f81f6edb45768d9
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2021-07-01 14:55:30 +0800

    Update scanner.js

--- menu/scanner.js
+++ menu/scanner.js
@@ -22,7 +22,7 @@ var textHandler = function(n) {
     A.target = "_page"
 		A.appendChild(NodeRef);
 		SongName = A.innerText;
-		SongReference = SongName.replace(SongName,'https://'+base+'/RadioAndMusicPlayer/media/'+SongName+'.mp3').replace(/&/g,'_and_');
+		SongReference = SongName.replace(SongName,'https://'+base+'/RadioAndMusicPlayer/media/'+SongName+'.mp3').replace(/&/mi,'_and_');
 		A.setAttribute('href', './index.html?mp3=' + SongReference);
 		//A.setAttribute('href', 'https://'+base+'/mp3player/?mp3=' + SongReference);
 		A.setAttribute('title', 'Click to Play: ' + SongName);
