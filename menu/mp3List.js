8cd4ebc76ea12e85484f6a48c4ca90143a371d62
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2021-07-29 12:53:38 +0800

    Update mp3List.js

--- menu/mp3List.js
+++ menu/mp3List.js
@@ -1,4 +1,5 @@
 var listMp3 = {
+  "Frida Wallin - When I Am Calling Your Name":"https://dirkncl.github.io/RadioAndMusicPlayer/media/Frida Wallin - When I Am Calling Your Name.mp3",
   "ABThree - Nyanyian Cintamu":"https://dirkncl.github.io/RadioAndMusicPlayer/media/ABThree - Nyanyian Cintamu.mp3",
   "Rama Aiphama - Dinda Bestari":"https://dirkncl.github.io/RadioAndMusicPlayer/media/Rama Aiphama - Dinda Bestari.mp3",
   "John Denver - Leaving On A Jet Plane":"https://dirkncl.github.io/RadioAndMusicPlayer/media/John Denver - Leaving On A Jet Plane.mp3",        
